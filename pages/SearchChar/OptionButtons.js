025d632c9bb048b9190603f28c7e6dc10da98bbd
Kwon Ohjin <lazy1344@naver.com>
2021-07-16 17:08:22 +0900

    Add filter and button state checked

--- pages/SearchChar/OptionButtons.js
+++ pages/SearchChar/OptionButtons.js
@@ -1,79 +1,104 @@
-import { ToggleButton, Button, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
-import { useState } from 'react';
+
+import { Button, ButtonToolbar, ButtonGroup } from 'react-bootstrap';
+import { useState, useRef } from 'react';
 import { attrOptions, classOptions, rarityOptions} from './ButtonOptions'
 
-
-// id="toggle-check"
-//           type="checkbox"
-//           variant="secondary"
-//           checked={checked}
-//           value="1"
-const MainAttrOptionButton = ({object, setOption}) => {
+const OptionButton = ({object, options, setOptions, optionKey}) => {
   const [checked, setChecked] = useState(false);
+  const [ buttonVariant, setButtonVariant ] = useState("secondary");
+
+  const setOption = (value, optionKey, options) => {
+    if(checked == false){
+      let valueArray = options[optionKey]
+      if(!(valueArray.includes(value))) {
+        valueArray = options[optionKey].concat(value);
+        const _options = { ...options, [optionKey]: valueArray };
+        console.log(valueArray);
+        setOptions(_options);
+        setChecked(true); 
+        setButtonVariant('dark');
+        return;
+      }
+    }
+      if(checked == true){
+        let valueArray = options[optionKey]
+        valueArray = options[optionKey].filter(val => val !== value);
+        const _options = { ...options, [optionKey]: valueArray };
+        console.log(valueArray);
+        setOptions(_options);
+        setChecked(false);
+        setButtonVariant("secondary");
+        return;
+      }
+  }
   return (
-    <ToggleButton id="toggle-check" type="checkbox" variant="secondary" value={object.value} onClick={() => setOption(object.value, "main_attribute", {checked})}><img src={object.icon}></img></ToggleButton>
+    <Button type="checkbox" variant={buttonVariant} value={object.value} onClick={() => setOption(object.value, optionKey, options)}><img src={object.icon}></img></Button>
   )
 }
 
-const SubAttrOptionButton = ({object, setOption}) => {  
-  const [checked, setChecked] = useState(true);
-  return (
-    <Button variant="secondary" value={object.value} onClick={() => setOption(object.value, "sub_attribute", {checked})}><img src={object.icon}></img></Button>
-  )
-}
-
-const RarityOptionButton = ({object, setOption}) => {
-  return (
-    <Button variant="secondary" value={object.value} onClick={() => setOption(object.value, "rarity")}>{object.icon}</Button>
-  )
-}
-
-const ClassOptionButton = ({object, setOption}) => {
-  return (
-    <Button variant="secondary" value={object.value} onClick={() => setOption(object.value, "class")}><img src={object.icon}></img></Button>
-  )
-}
-
-const OptionButtons = ({options, setOption}) => {
+const RarityOptionButton = ({object, options, setOptions, optionKey}) => {
   const [checked, setChecked] = useState(false);
-  const [radioValue, setRadioValue] = useState('1');
-  const [value, setValue] = useState([1, 3]);
-  const handleChange = (val) => setValue(val);
+  const [ buttonVariant, setButtonVariant ] = useState("secondary");
 
-  const radios = [
-    { name: 'Active', value: '1' },
-    { name: 'Radio', value: '2' },
-    { name: 'Radio', value: '3' },
-  ];
+  const setOption = (value, optionKey, options) => {
+    if(checked == false){
+      let valueArray = options[optionKey]
+      if(!(valueArray.includes(value))) {
+        valueArray = options[optionKey].concat(value);
+        const _options = { ...options, [optionKey]: valueArray };
+        console.log(valueArray);
+        setOptions(_options);
+        setChecked(true); 
+        setButtonVariant('dark');
+        return;
+      }
+    }
+      if(checked == true){
+        let valueArray = options[optionKey]
+        valueArray = options[optionKey].filter(val => val !== value);
+        const _options = { ...options, [optionKey]: valueArray };
+        console.log(valueArray);
+        setOptions(_options);
+        setChecked(false);
+        setButtonVariant("secondary");
+        return;
+      }
+  }
+  return (
+    <Button type="checkbox" variant={buttonVariant} value={object.value} onClick={() => setOption(object.value, optionKey, options)}>{object.icon}</Button>
+  )
+}
+
+
+const OptionButtons = ({options, setOptions, setOption}) => {
   return(
     <div>
       <ButtonToolbar>
         <ButtonGroup className="mb-2">
-          <MainAttrOptionButton object={attrOptions.fire} options={options} setOption={setOption}></MainAttrOptionButton>
-          <MainAttrOptionButton object={attrOptions.water} options={options} setOption={setOption}></MainAttrOptionButton>
-          <MainAttrOptionButton object={attrOptions.forest} options={options} setOption={setOption}></MainAttrOptionButton>
-          <MainAttrOptionButton object={attrOptions.thunder} options={options} setOption={setOption}></MainAttrOptionButton>
+          <OptionButton object={attrOptions.fire} options={options} setOptions={setOptions} optionKey={"main_attribute"}></OptionButton>
+          <OptionButton object={attrOptions.water} options={options} setOptions={setOptions} optionKey={"main_attribute"}></OptionButton>
+          <OptionButton object={attrOptions.forest} options={options} setOptions={setOptions} optionKey={"main_attribute"}></OptionButton>
+          <OptionButton object={attrOptions.thunder} options={options} setOptions={setOptions} optionKey={"main_attribute"}></OptionButton>
         </ButtonGroup>
-        <br></br>
-        <ButtonGroup className="me-2">
-          <SubAttrOptionButton object={attrOptions.fire} options={options} setOption={setOption}></SubAttrOptionButton>
-          <SubAttrOptionButton object={attrOptions.water} options={options} setOption={setOption}></SubAttrOptionButton>
-          <SubAttrOptionButton object={attrOptions.forest} options={options} setOption={setOption}></SubAttrOptionButton>
-          <SubAttrOptionButton object={attrOptions.thunder} options={options} setOption={setOption}></SubAttrOptionButton>
+        <ButtonGroup className="mb-2">
+          <OptionButton object={attrOptions.fire} options={options} setOptions={setOptions} optionKey={"sub_attribute"}></OptionButton>
+          <OptionButton object={attrOptions.water} options={options} setOptions={setOptions} optionKey={"sub_attribute"}> </OptionButton>
+          <OptionButton object={attrOptions.forest} options={options} setOptions={setOptions} optionKey={"sub_attribute"}></OptionButton>
+          <OptionButton object={attrOptions.thunder} options={options} setOptions={setOptions} optionKey={"sub_attribute"}></OptionButton>
         </ButtonGroup>
       </ButtonToolbar>
       <ButtonToolbar> 
-        <ButtonGroup className="me-2">
-          <ClassOptionButton object={classOptions.detonator} options={options} setOption={setOption}></ClassOptionButton>
-          <ClassOptionButton object={classOptions.sniper} options={options} setOption={setOption}></ClassOptionButton>
-          <ClassOptionButton object={classOptions.converter} options={options} setOption={setOption}></ClassOptionButton>
-          <ClassOptionButton object={classOptions.support} options={options} setOption={setOption}></ClassOptionButton>
+        <ButtonGroup className="mb-2">
+          <OptionButton object={classOptions.detonator} options={options} setOptions={setOptions} optionKey={"class"}></OptionButton>
+          <OptionButton object={classOptions.sniper} options={options} setOptions={setOptions} optionKey={"class"}></OptionButton>
+          <OptionButton object={classOptions.converter} options={options} setOptions={setOptions} optionKey={"class"}></OptionButton>
+          <OptionButton object={classOptions.support} options={options} setOptions={setOptions} optionKey={"class"}></OptionButton>
         </ButtonGroup>
-        <ButtonGroup className="me-2">
-          <RarityOptionButton object={rarityOptions.three} options={options} setOption={setOption}></RarityOptionButton>
-          <RarityOptionButton object={rarityOptions.four} options={options} setOption={setOption}></RarityOptionButton>
-          <RarityOptionButton object={rarityOptions.five} options={options} setOption={setOption}></RarityOptionButton>
-          <RarityOptionButton object={rarityOptions.six} options={options} setOption={setOption}></RarityOptionButton>        
+        <ButtonGroup className="mb-2">
+          <RarityOptionButton object={rarityOptions.three} options={options} setOptions={setOptions} optionKey={"rarity"}></RarityOptionButton>
+          <RarityOptionButton object={rarityOptions.four} options={options} setOptions={setOptions} optionKey={"rarity"}></RarityOptionButton>
+          <RarityOptionButton object={rarityOptions.five} options={options} setOptions={setOptions} optionKey={"rarity"}></RarityOptionButton>
+          <RarityOptionButton object={rarityOptions.six} options={options} setOptions={setOptions} optionKey={"rarity"}></RarityOptionButton>        
         </ButtonGroup>
       </ButtonToolbar>
     </div>
