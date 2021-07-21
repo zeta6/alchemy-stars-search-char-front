00d5baa7ddc068c56472fe731977b3b5f4eabea1
Kwon Ohjin <lazy1344@naver.com>
2021-07-21 18:51:59 +0900

    add bootstrap grid

--- pages/CharacterList/CharacterList.js
+++ pages/CharacterList/CharacterList.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef, useReducer} from 'react';
-import { Table, ButtonGroup, Button} from 'react-bootstrap';
+import { Table, ButtonGroup, Button, Row, Col, ButtonToolbar} from 'react-bootstrap';
 import CharacterInList from './CharacterInList';
 import axios from 'axios';
 
@@ -61,14 +61,13 @@ const CharacterList = ({options}) => {
 
   useEffect(() => {
     async function fetchData(){
+    const filter = characterFilter;
     const response = await axios.get("api/characters");
-    setCharacterList(response.data.filter(characterFilter));
-    setCurrentList(response.data.filter(characterFilter).slice(0,5))
-    setPages(Array((parseInt(response.data.filter(characterFilter).length / characterPerPage) + 1))
+    setCharacterList(response.data.filter(filter));
+    setCurrentList(response.data.filter(filter).slice(0,5))
+    setPages(Array((parseInt(response.data.filter(filter).length / characterPerPage) + 1))
     .fill(1).map((x,y) => x + y ));
     setCurrentPage(1)
-    
-    console.log('pages',pages)
   }
   fetchData();
 },[options]
@@ -109,17 +108,20 @@ const CharacterList = ({options}) => {
   else{
   return(
     <div>
+    <Row className="character-list">
       {/* <button onClick={() => a_test()}>test!</button> */}
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
-            <th>open</th>
-            <th>Name</th>
-            <th>Rarity</th>
-            <th>M_Attr</th>
-            <th>S_Attr</th>
-            <th>Class</th>
-            <th>detail</th>
+            <th></th>
+            <th>아이콘</th>
+            <th>이름</th>
+            <th>성급</th>
+            <th>주속성</th>
+            <th>부속성</th>
+            <th>클래스</th>
+            <th>세력</th>
+            <th>캐릭터페이지</th>
           </tr>
         </thead>
         {currentList.map((cha)=> ( 
@@ -129,15 +131,18 @@ const CharacterList = ({options}) => {
           </React.Fragment>
         ))}
       </Table>
-      <div>
-        <ButtonGroup>
-        pages : {pages.map((page) => (
+    </Row>
+    <Row className="page-button-tool-bar-row">
+    <ButtonToolbar>
+      <ButtonGroup className="page-button-group">
+        {pages.map((page) => (
           <React.Fragment key={page}>
             <PageButton page={page} handlePage={handlePage} currentPage={currentPage}></PageButton>
           </React.Fragment>
-        ))}
-        </ButtonGroup>
-      </div>
+       ))}
+      </ButtonGroup>
+    </ButtonToolbar>
+    </Row>
     </div>
   )
         }
