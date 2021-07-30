60e6688836656f74b412f79c5f1ae303ecd95ce6
Kwon Ohjin <lazy1344@naver.com>
2021-07-30 10:18:04 +0900

    fix misstype

--- pages/CharacterList/[characterInfo].js
+++ pages/CharacterList/[characterInfo].js
@@ -69,7 +69,7 @@ export default function CharacterInfo(){
       <Container className="character-info-container">
         <Row>
           <Col className="character-info-first-row-index-col">
-            바로가기 : <a href="#quickView">퀵뷰</a> / <a href="#equipinfo">장비정보</a> / <a href="#profile">프로필</a> <a href="#breakthrough">한계돌파</a> / <a href="#ascension">각성</a> / <a href="#preferredGifts">선호선물</a> / <a href="#characterFile">캐릭터파일</a> / 
+            바로가기 : <a href="#quickView">퀵뷰</a> / <a href="#equipinfo">장비정보</a> / <a href="#profile">프로필</a> / <a href="#breakthrough">한계돌파</a> / <a href="#ascension">각성</a> / <a href="#preferredGifts">선호선물</a> / <a href="#characterFile">캐릭터파일</a> / 
           </Col>
         </Row>
         <Row>
