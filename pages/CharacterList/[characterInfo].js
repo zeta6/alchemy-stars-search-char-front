0a10eeb172ec9aa49a8fb6dfc670d1998c97a13c
Kwon Ohjin <lazy1344@naver.com>
2021-07-31 05:34:58 +0900

    Fix charInfo useEffect to get router queries

--- pages/CharacterList/[characterInfo].js
+++ pages/CharacterList/[characterInfo].js
@@ -15,15 +15,17 @@ import Image from "next/image"
 
 export default function CharacterInfo(){
   const router = useRouter();
+  const character_id = router.query.characterInfo;
   const [ character, setCharacter ] = useState(CharacterState)
   const [ loading , setLoading ] = useState(true)
   
   useEffect(() => {
-    axios.get(`https://alchemystars.link:8715/api/character/${router.query.characterInfo}/`)
+    if(character_id){
+    axios.get(`https://alchemystars.link:8715/api/character/${character_id}/`)
       .then(response => setCharacter(response.data))
       .catch(error => console.log(error));
       setLoading(false)
-    },[router.query.characterInfo]
+    }},[character_id]
   );
 
   const [ chainSkill, setChainSkill ] = useState("first")
