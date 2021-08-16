c97d5baefc7626e5611bcddd6e436c5714d94d6f
Kwon Ohjin <lazy1344@naver.com>
2021-08-16 17:23:34 +0900

    edit backend url

--- components/BackendUrl.js
+++ components/BackendUrl.js
@@ -1,2 +1,2 @@
-// export const BackendUrl = "https://alchemystars.link:8715"
-export const BackendUrl = "http://localhost:8000"
+export const BackendUrl = "https://alchemystars.link:8715"
+// export const BackendUrl = "http://localhost:8000"
