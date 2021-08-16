3779a41ef50e428b55c94a08bfac3e02077c3c9a
Kwon Ohjin <lazy1344@naver.com>
2021-08-17 02:52:20 +0900

    Add active preemptive indication when breakthrough

--- pages/CharacterList/character-info/ActiveSkillView.js
+++ pages/CharacterList/character-info/ActiveSkillView.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import {Row, Col, Card} from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
+import {Row, Col, Card, CardGroup} from 'react-bootstrap';
 import Image from "next/image";
 
-const ActiveSkillView = ({skill}) => {
+const ActiveSkillView = ({skill, rarity}) => {  
+  const [ basicPreemptive, setBasicPreempTive ] = useState("")
+  const [ brthPreemptive, setBrthPreempTive ] = useState("")
+  const [ upgSkillBrth, setUpgSkillBrth ] = useState(["",""])
+
+  useEffect(() => {
+    if(rarity){
+      if(rarity == 3){
+        setUpgSkillBrth(["3"])
+      }else if(rarity == 4){
+        setUpgSkillBrth(["4"])
+      }else if(rarity == 5){
+        setUpgSkillBrth(["2","5"])
+      }else if(rarity == 6){
+        setUpgSkillBrth(["3","6"])
+      }
+    }
+  }, [rarity])
+
+  useEffect(() => {
+    if(skill){
+      if(skill.preemptive == "X"){
+        setBasicPreempTive("X")
+        setBrthPreempTive("X")
+      }
+      if(skill.preemptive == "O"){
+        setBasicPreempTive("O")
+        setBrthPreempTive("O")
+      }else if(skill.preemptive == 1){
+        setBasicPreempTive("X")
+        setBrthPreempTive(upgSkillBrth[0])
+      }else if(skill.preemptive == 2){
+        setBasicPreempTive("X")
+        setBrthPreempTive(upgSkillBrth[1])
+      }
+    }
+  },[upgSkillBrth])
+
   if(!skill){
     return null;
   }
@@ -12,20 +49,19 @@ const ActiveSkillView = ({skill}) => {
       <Col lg={12}>
         액티브: {skill.name}
       </Col>
-      <Col lg={4}>
+      <Col lg={3}>
         <Image unoptimized="true" width="60" height="60" src={skill.icon} alt="skill.icon"></Image>
       </Col>
-      <Col xs={4} lg={3}>
+      <Col xs={12} lg={8}>
         <div className="active-skill-card-wrap">
           <Card className="active-skill-card">
             CD:{skill.cooltime}
           </Card>
-        </div>
-      </Col>
-      <Col xs={4} lg={4}>
-        <div className="active-skill-card-wrap">
-          <Card className="active-skill-card">
-            선제:{skill.preemptive}
+          <Card className="active-skill-card-text">
+            선제:{basicPreemptive}
+          </Card>
+          <Card className="active-skill-card-text-long">
+            돌파선제:{brthPreemptive}
           </Card>
         </div>
       </Col>
