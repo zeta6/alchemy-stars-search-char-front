c2fb31425f43d862e0c5ee3a08f701fa75d95dd2
Kwon Ohjin <61285931+zeta6@users.noreply.github.com>
2021-08-17 12:12:58 +0900

    Delete BackendUrl.js

--- components/BackendUrl.js
+++
@@ -1,4 +0,0 @@
-// export const BackendUrl = "https://alchemystars.link:8715"
-export const BackendUrl = "http://localhost:8000"
-
-export const googleClientID = '571135633127-1n71sneiab3vc1vp87e4imslv9slg377.apps.googleusercontent.com';
